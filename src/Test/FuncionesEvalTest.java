6ce76705d4f0da8bc076101f5ef0144fab058aa4
carmona.ceenr <enriquecarmonacerrato@gmail.com>
2026-04-24 16:34:47 +0200

    Primer commmit antes de probare los test

---
+++ src/Test/FuncionesEvalTest.java
@@ -0,0 +1,26 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FuncionesEvalTest {
+
+    @org.junit.jupiter.api.Test
+    void esPerfecto() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTipoClima() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void contarApariciones() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void invertirArray() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void encontrarMayor() {
+    }
+}
